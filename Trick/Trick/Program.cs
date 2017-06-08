cc5816c13d7b46a84b34354d22a949d8a7c902f8
MD Shohag Mia <shohaghassan14@hotmail.com>
2017-06-08 23:41:17 +0600

    Initial Commit

---
+++ Trick/Trick/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Trick
+{
+    class Program
+    {
+        public static void Removesss(List<int> values)
+        {
+            values = values.Where(x => x != 0).ToList();
+        }
+        static void Main()                                  //Errorr Solution of Problem in Main1() Replace by Main() 
+        {
+            List<int> Myvalues = new List<int> { 1, 0, 3, 4, 0, 6, 0 };
+
+            Removesss(Myvalues);
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
