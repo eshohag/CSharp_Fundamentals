ccbea132462ecca8ad1abff5f4b1c9a9ca4f235f
MD. Shohag Mia <mdshohagmia14@outlook.com>
2017-07-05 19:35:00 +0600

    Nothing

--- 00TestApps/00TestApps/Program.cs
+++ 00TestApps/00TestApps/Program.cs
@@ -6,7 +6,7 @@ namespace _00TestApps
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!");
         }
     }
 }
