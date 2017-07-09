c40858275814ff585320303e397b9c40f35857d3
MD. Shohag Mia <mdshohagmia14@outlook.com>
2017-07-09 21:25:10 +0600

    ReleaseCommit

--- SqlServerConnections/SqlServerConnections/SqlServerConnection.cs
+++ SqlServerConnections/SqlServerConnections/SqlServerConnection.cs
@@ -3,27 +3,27 @@ using System.Data.SqlClient;
 
 namespace SqlServerConnections
 {
-    public class SqlServerConnection
+    public abstract class SqlServerConnection
     {
-        public abstract class SqlConnections
-        {
-            public static SqlConnection Connection(string serverName, string databaseName)
-            {
-                String connectionString = @"server='" + serverName + "';database='" + databaseName + "';Integrated Security=True";
-                SqlConnection connection = new SqlConnection(connectionString);
-                return connection;
 
-            }
-            public static int Connection(string serverName, string databaseName, string insertOrUpdateQuery)
-            {
-                String connectionString = @"server='" + serverName + "';database='" + databaseName + "';Integrated Security=True";
-                SqlConnection connection = new SqlConnection(connectionString);
-                SqlCommand command = new SqlCommand(insertOrUpdateQuery, connection);
-                connection.Open();
-                int rowAffected = command.ExecuteNonQuery();
-                connection.Close();
-                return rowAffected;
-            }
+        public static SqlConnection Connection(string serverName, string databaseName)
+        {
+            String connectionString = @"server='" + serverName + "';database='" + databaseName + "';Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+
         }
+        public static int Connection(string serverName, string databaseName, string insertOrUpdateQuery)
+        {
+            String connectionString = @"server='" + serverName + "';database='" + databaseName + "';Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(insertOrUpdateQuery, connection);
+            connection.Open();
+            int rowAffected = command.ExecuteNonQuery();
+            connection.Close();
+            return rowAffected;
+        }
+
+
     }
 }
