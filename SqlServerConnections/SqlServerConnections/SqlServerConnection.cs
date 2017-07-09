95276b417c2542c3135e97f05952d7ecfa2f66ae
MD. Shohag Mia <mdshohagmia14@outlook.com>
2017-07-09 21:09:14 +0600

    initialCommit

---
+++ SqlServerConnections/SqlServerConnections/SqlServerConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SqlServerConnections
+{
+    public class SqlServerConnection
+    {
+        public abstract class SqlConnections
+        {
+            public static SqlConnection Connection(string serverName, string databaseName)
+            {
+                String connectionString = @"server='" + serverName + "';database='" + databaseName + "';Integrated Security=True";
+                SqlConnection connection = new SqlConnection(connectionString);
+                return connection;
+
+            }
+            public static int Connection(string serverName, string databaseName, string insertOrUpdateQuery)
+            {
+                String connectionString = @"server='" + serverName + "';database='" + databaseName + "';Integrated Security=True";
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(insertOrUpdateQuery, connection);
+                connection.Open();
+                int rowAffected = command.ExecuteNonQuery();
+                connection.Close();
+                return rowAffected;
+            }
+        }
+    }
+}
